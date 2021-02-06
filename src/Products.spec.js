9f3581e9cf703fa726354db0f0e41ac98b41cff7
Joan Morató <moratojoan@gmail.com>
2021-02-06 13:12:29 +0100

    test: make 'render list of products' not depend on real data

--- src/Products.spec.js
+++ src/Products.spec.js
@@ -1,10 +1,19 @@
 import { render } from '@testing-library/react';
-import App from './App';
+import Products from './Products';
 
+
+const products = [
+    {
+        id: "product-1",
+        name: "Product 1",
+        image: "/image.jpg",
+        price: 10
+    }
+];
 
 describe("product list", () => {
     it("render list of products", () => {
-        const container = render(<App />)
+        const container = render(<Products products={products} />)
         expect(container.getByText("Product 1")).toBeInTheDocument()
     })
-})
+});
